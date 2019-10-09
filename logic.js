f4f7873d99876b02372b9480590bb2a21f4ddcec
Nicholas Kinney <kinneyanick@gmail.com>
2019-10-09 00:19:56 -0500

    Animation of noise

--- logic.js
+++ logic.js
@@ -6,6 +6,39 @@ canvas.style = "position: fixed; top: 0; left: 0;";
 document.body.append(canvas); 
 
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#000000";
-ctx.fillRect(0,0, canvas.width, canvas.height);
+var NG;
+var octaves = 4;
 
+let noiseScale;
+
+let time = 0;
+let drawInterval;
+const adjustment = Math.PI*255/2;
+
+(function init(){
+    NG = new NoiseGenerator([255, 255, 20]);
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+
+    noiseScale = [0.006, 0.006];
+    drawInterval = setInterval(draw, 1000);
+})();
+
+function draw(){
+    time += 0.025;
+    const s = 4;
+    for(let y = 0; y < canvas.height; y+= s){
+        for(let x = 0; x < canvas.width; x+=s){
+            let color = Math.floor(constrain(map(new Array(octaves).fill(0).reduce((acc, _, index) => {
+                let val = NG.noise([x*noiseScale[0]*index+adjustment*index, y*noiseScale[1]*index+adjustment*index, time]);
+                return acc = acc? acc+val*Math.pow(0.75, index): val;
+            }), -0.75, 0.75, 0, 255), 0, 255))
+            ctx.fillStyle = `rgb(${color}, ${color}, ${color})`;
+            rect(x, y, s, s);
+        }
+    }
+}
+
+function rect(x, y, w, h){
+    ctx.fillRect(x, y, w, h);
+}
